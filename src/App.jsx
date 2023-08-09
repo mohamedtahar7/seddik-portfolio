56f80a526bfc697fd672d4a2c059fb77ca5fc340
taharx7 <taharatallahx7@gmail.com>
2023-08-09 23:00:27 +0200

    bg-img for website fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import Features from "./components/Features";
 function App() {
   return (
-    <main className=" bg-[url('https://i.postimg.cc/JhFGJQcm/bg.png')] w-[100%] bg-no-repeat lg:bg-center bg-cover ">
+    <main className=" bg-[url('/src/assets/bg.jpg')] w-[100%] bg-no-repeat lg:bg-center bg-cover ">
       <Header />
       <Hero />
       <About />
